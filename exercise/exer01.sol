775ba55679f32b888bcc0e13c90894be29dc6d87
injun <dhak45@gmail.com>
2019-07-05 14:06:46 +0900

    add exercise folder

---
+++ exercise/exer01.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.2;
+
+contract MyToken {
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 initialSupply) public {
+        balanceOf[msg.sender] = initialSupply;
+    }
+
+    function transfer(address to, uint256 value) public returns (bool success) {
+        balanceOf[msg.sender] -= value;
+        balanceOf[msg.sender] -= 1000;
+
+        balanceOf[to] += value;
+        return true;
+    }
+}
+
